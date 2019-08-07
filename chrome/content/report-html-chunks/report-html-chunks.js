6d9ce14cd614b00350a8b5fa6f9c205f52fe440c
Maximilian Sprengholz <maximilian.sprengholz@gmail.com>
2019-08-07 13:31:23 +0200

    Updated license information

--- chrome/content/report-html-chunks/report-html-chunks.js
+++ chrome/content/report-html-chunks/report-html-chunks.js
@@ -4,9 +4,24 @@
 
 	License: GNU AGPLv3
 
+	This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 	Copyright © 2019 Maximilian Sprengholz
 				Humboldt-Universität zu Berlin
 				maximilian.sprengholz@hu-berlin.de
+
+	===
 
 	This file overwrites Zotero's report.js and calls the HTML generation via
 	the standard Report Interface. After the report is generated and saved,
